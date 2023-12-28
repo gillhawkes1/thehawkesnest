332c893e713ad2808f56ab899a9703073a21ae8d
Gill Hawkes <gillhawkes1@gmail.com>
2023-12-28 10:34:22 -0500

    added echo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include_once('config.php');
 $sqlconn = new mysqli($THN_HOST,$THN_USER,$THN_PASSWORD,$THN_DB_NAME);
 
 if($sqlconn->connect_error) {
+  echo 'connection failed';
   die("connection failure " . $conn->connect_error);
 }
 echo "connected to db";
