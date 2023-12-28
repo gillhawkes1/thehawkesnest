62237d42c07e18336b2d700f9bb84f4bae491921
Gill Hawkes <gillhawkes1@gmail.com>
2023-12-28 12:33:01 -0500

    testing import

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 include_once('config.php');
-$sqlconn = new mysqli($THN_HOST,$THN_USER,$THN_PASSWORD,$THN_DB_NAME);
+echo $THN_USER;
+// $sqlconn = new mysqli($THN_HOST,$THN_USER,$THN_PASSWORD,$THN_DB_NAME);
 
-if($sqlconn->connect_error) {
-  echo 'connection failed';
-  die("connection failure " . $conn->connect_error);
-}
-echo "connected to db";
-$sqlconn->close();
-?>
+// if($sqlconn->connect_error) {
+//   echo 'connection failed';
+//   die("connection failure " . $conn->connect_error);
+// }
+// echo "connected to db";
+// $sqlconn->close();
+?> 
