1fad31b9af44b560d1a7ea8c839cf186809ad6fd
Gill Hawkes <gillhawkes1@gmail.com>
2023-12-28 10:31:03 -0500

    trying to connect to db

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
+include_once('config.php');
+$sqlconn = new mysqli($THN_HOST,$THN_USER,$THN_PASSWORD,$THN_DB_NAME);
 
-echo "hello the hawkes nest!"
-
+if($sqlconn->connect_error) {
+  die("connection failure " . $conn->connect_error);
+}
+echo "connected to db";
+$sqlconn->close();
 ?>
