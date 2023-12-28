1d5512f4f358e386f4b56b6165ca6ab19593f576
Gill Hawkes <gillhawkes1@gmail.com>
2023-12-28 13:11:18 -0500

    validation of import

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 include('/var/www/config.php');
+echo 'included config.php successfully';
 
 $sqlconn = new mysqli($THN_HOST,$THN_USER,$THN_PASSWORD,$THN_DB_NAME);
 
