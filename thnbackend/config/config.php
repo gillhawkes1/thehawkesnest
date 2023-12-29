37abc864f270f14f609060c27d1f421075800665
Gill Hawkes <gillhawkes1@gmail.com>
2023-12-29 09:52:15 -0500

    rm cached file for gitignore

--- thnbackend/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('THN_HOST', 'localhost');
-define('THN_USER', 'gill');
-define('THN_PASSWORD', 'yReKLP3-xCL3yVw82dNN!-j.WBCgEsoEW@T3_RkYtu49omFEjBJKsAti*94o84QZ4jBeKfT*k!@XJMRNMM3xZbs7LruUPq*gk4ox');
-define('THN_DB_NAME', 'thehawkesnest');
-?>
