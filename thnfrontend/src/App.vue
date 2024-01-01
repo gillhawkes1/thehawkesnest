8e38a9a7112b868bf75daabe8799615f951680f4
Gill Hawkes <gillhawkes1@gmail.com>
2024-01-01 15:18:52 -0500

    construction zone

--- thnfrontend/src/App.vue
+++ thnfrontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UnderConstruction msg="Powered by Vuejs. This site is underway!"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UnderConstruction from './components/UnderConstruction.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UnderConstruction
   }
 }
 </script>
