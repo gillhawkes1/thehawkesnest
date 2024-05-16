de21ff616dbfdade648bcc9ceaedbe5f6c8d1395
Gill Hawkes <gillhawkes1@gmail.com>
2024-05-16 08:40:44 -0400

    updated gitignore for local testing

--- thnbackend/public/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//local credentials for testing, prod credentials private
-define('THN_HOST', 'localhost');
-define('THN_USER', 'root');
-define('THN_PASSWORD', 'NCODE95K');
-define('THN_DB_NAME', 'thehawkesnesttesting');
